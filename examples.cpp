4cfe600ced4edba5b548104b938f84c515dbbba9
MrEidam <adammbrich@gmail.com>
2023-10-09 15:11:09 +0200

    job done

--- examples.cpp
+++ examples.cpp
@@ -1,111 +1,84 @@
 #include <iostream>
-/*
-int sum(int a, int b){
-    return a + b;
-}
-int sub(int a, int b){
-    return a - b;
-}
+#include <string>
 
-int a = 0;
-
-void scope(){
-    int a = 5; 
-
-    a = 3;
-    std::cout << a << std::endl; // 3
-    
-    {
-        int a = 4;
-        std::cout << a << std::endl; // 4
-        a = 6;
-        std::cout << a << std::endl; // 6
-    }
-
-    std::cout << a << std::endl; // 3
-
-}
-
-void cykly(){
-    for(int i = 0; i < 10; i++){
-        std::cout << i << std::endl;
-    }
-
-    int i = 0;
-    while(i < 10){
-        std::cout << i << std::endl;
-        i++;
-    }
-
-    i = 0;
-    do{
-        std::cout << i << std::endl;
-        i++;
-    }while(i < 10);
-}*/
-void stars(unsigned int star){
+void stars(int star){
     while(star != 0){
         std::cout << "⬜";
         star--;
     }
 }
 
-void rectangle(unsigned int height,unsigned int width){
+void rectangle(int height,int width){
     std::cout << std::endl;
     std::string str= "";
-    for(unsigned int i = 0; i < width; i++){
+    for(int i = 0; i < width; i++){
         str += "⬜";
     }
-    for(unsigned int i = 0; i < height; i++){
+    for(int i = 0; i < height; i++){
         std::cout << str << std::endl;
     }
 }
 
-void lpyramid(unsigned int size){
+void lpyramid(int size){
     std::cout << std::endl;
     std::string lpyr = "";
-    for(unsigned int i = 0; i < size; i++){
+    for(int i = 0; i < size; i++){
         lpyr += "⬜";
         std::cout << lpyr << std::endl;
     }
 }
-void spyramid(unsigned int size){
+void spyramid(int size){
     std::cout << std::endl;
     std::string lpyr = "";
-    for(unsigned int i = 0; i < size; i++){
+    for(int i = 0; i < size-1; i++){
         lpyr += "⬜";
         std::cout << lpyr << std::endl;
     }
-    for(unsigned int i = 0; i < size; i++){
+    for(int i = 0; i < size; i++){
         int numb = size-i;
         lpyr = "";
-        while(numb < 0){
+        while(numb > 0){
             lpyr += "⬜";
             numb--;
         }
         std::cout << lpyr << std::endl;
     }
 }
-void cpyramid(unsigned int size){
+void cpyramid(int size){
     std::cout << std::endl;
     std::string cpyr= "";
 
-    for(unsigned i = 0; i < size; i++){
-        // cpyr = (i+1)*size;
+    for(int i = 0; i < size; i++){
+        int b = size - i;
+        for(int i = 0; i < b-1; i++){
+            std::cout << "  ";
+        }
+        for(int i= 0; i <= (size-b)*2; i++){
+            std::cout << "⬜";
+        }
         std::cout << std::endl;
     }
 }
 
-unsigned int num0 = 0;
-unsigned int num1 = 0;
+int num0 = 0;
+int num1 = 0;
 int main(){
-    // std::cout << "How many stars? ";
-    // std::cin >> num1;
-    // stars(num1);
-    // std::cout << std::endl;
-    // rectangle(6,9);
-    // std::cout << std::endl;
-    // lpyramid(10);
-    spyramid(5);
+    std::cout << "How many stars: ";
+    std::cin >> num1;
+    stars(num1);
+    std::cout << "\nWidth of your rectangle: ";
+    std::cin >> num1;
+    std::cout << "\nHeight of your rectangle: ";
+    std::cin >> num0;
+    rectangle(num0,num1);
+    std::cout << "\nLevels of your triangle: ";
+    std::cin >> num0;
+    lpyramid(num0);
+    std::cout << "\nLevels of your triangle: ";
+    std::cin >> num0;
+    spyramid(num0);
+    std::cout << "\nLevels of your triangle: ";
+    std::cin >> num0;
+    cpyramid(num0);
     return 0;
 }
